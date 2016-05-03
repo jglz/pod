3c0c5dcbc96b2d691a0aedb60838f5670c30de42
ynagxiaobing <jiaogelangzhong@jiaoglz.com>
2016-05-03 14:31:50 +0800

    pod
    pod

---
+++ People.swift
@@ -0,0 +1,14 @@
+//
+//  People.swift
+//  pod
+//
+//  Created by jglz on 16/5/3.
+//  Copyright © 2016年 yxb. All rights reserved.
+//
+
+import UIKit
+
+class People: NSObject {
+
+    var name = ""
+}
